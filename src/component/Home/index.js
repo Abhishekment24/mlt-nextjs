eb19cbf4e41e86ccb8645412cad76d99db7409e5
Abhishek Raj <abhishekraj@Abhisheks-Air.bbrouter>
2023-06-09 19:14:48 +0530

    done

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -12,8 +12,8 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Home Page</title>
-        <meta name="description" content="Home Page" />
+        <title>Regulated Blockchain & AI Products & Services Provider</title>
+        <meta name="description" content=" Regulated Blockchain & AI Products & Services Provider" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
